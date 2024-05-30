dbf4d920a28d269174b498ea36775e66ee642eec
Jakub Korycki <jkorycki@tudelft.nl>
2024-05-30 15:31:19 +0200

    fixed wavefront?! maybe?

--- Part_5_fft_interp.py
+++ Part_5_fft_interp.py
@@ -10,14 +10,18 @@ from camera.ueye_camera import uEyeCamera
 from dm.okotech.dm import OkoDM
 import time
 import os
-
+from zernike import RZern
+import sys
 from pathlib import Path
 os.chdir(Path(__file__).parent)
-from zernike import RZern
 from zern.zern import *
+from scipy.fftpack import fft2, fftshift, ifftshift, ifft2
+
 
 # Set the path for DLL loading
-os.environ['PATH'] = "C:\\AO-course-2024\\dm\\okotech\\okodm_sdk\\python" + os.pathsep + os.environ['PATH']
+#os.environ['PATH']= "C:\\AO-course-2024\\dm\\okotech\\okodm_sdk\\python" + os.pathsep + os.environ['PATH']
+sys.path.append("C:\\AO-course-2024\\dm\\okotech\\okodm_sdk\\python")
+
 
 class Camera:
     def __init__(self, camera_index, exposure):
@@ -59,9 +63,9 @@ def detect_blobs(img, show=False):
     params.minArea = 25
     params.maxArea = 1000
     params.filterByCircularity = True
-    params.minCircularity = 0.1
+    params.minCircularity = 0.5
     params.filterByConvexity = True
-    params.minConvexity = 0.5
+    params.minConvexity = 0.3
     params.filterByInertia = True
     params.minInertiaRatio = 0.01
     detector = cv2.SimpleBlobDetector_create(params)
@@ -126,15 +130,12 @@ def filter_points_by_neighbors(points, min_neighbours=4, neighbour_distance=65):
     filtered_points = points[neighbours_count >= min_neighbours]
     return filtered_points
 
-def normalize_dots_to_unit_circle(dots, padding=0.1, center =None, scale=None,):
-    if center is None:
-        center = np.mean(dots, axis=0)
-    dots_centered = dots - center
-    if scale is None:
-        scale = (1-padding)/np.max(np.sqrt(np.sum(dots_centered**2, axis=1)))
-    dots_scaled = dots_centered*scale
-    return dots_scaled, center, scale
-
+def compute_fft_image(image):
+    fft_result = fft2(image)
+    fft_result_shift = fftshift(fft_result)
+    fft_magnitude = fft_result_shift.real
+    fft_phase = fft_result_shift.imag
+    return fft_magnitude, fft_phase
 
 if __name__ == "__main__":
     plt.close('all')
@@ -152,11 +153,12 @@ if __name__ == "__main__":
             optimized_act = np.loadtxt(f"C:\\AO-course-2024\\part_4\\last_x.dat")[0]
 #            optimized_act[0]=1
             dm.setActuators(optimized_act)
-            dm.setActuators(np.random.rand(19) * 1.6 - 0.8)
+            dm.setActuators(np.random.rand(19) * 2 - 1)
             time.sleep(0.2)
             random_img = normal_camera.grab_frames(4)[-1]
             random_SH_img = sh_camera.grab_frames(4)[-1]
             random_dots = detect_blobs(random_SH_img, show=False)
+#            random_dots = reference_dots
             reference_dots = filter_points_by_neighbors(reference_dots)
             random_dots = filter_points_by_neighbors(random_dots)
             print(np.max(reference_img), np.max(reference_SH_img), np.max(random_img), np.max(random_SH_img))
@@ -165,67 +167,108 @@ if __name__ == "__main__":
         
         
 #%%
+plt.clf()
+#plt.clear()
+        
+def coords_to_img(coords):
+    s = int(np.max(coords)+20)
+    image = np.zeros((s, s), dtype=np.uint8)
+    for x,y in coords:
+        image[int(y), int(x)] = 255
+    return image
 
-    plt.clf()
-    reference_dots, random_dots = NearestNeighbor(reference_dots, random_dots)
-    reference_dots, center, scale = normalize_dots_to_unit_circle(reference_dots)
-    random_dots, _, _ = normalize_dots_to_unit_circle(random_dots, center=center, scale=scale)
-    print(len(reference_dots), len(random_dots))
-    plot_displacement(reference_dots, random_dots)
-    
-    #%%
-    
-    
-    displacements = reference_dots - random_dots
-    ''' Normalization to unit circle happens in cell above!'''
+plt.clf()
+reference_dots, random_dots = NearestNeighbor(reference_dots, random_dots)
+print(len(reference_dots), len(random_dots))
+plot_displacement(reference_dots, random_dots)
+plt.show()
+#%%
+plt.clf()
+refimg = coords_to_img(reference_dots)
+fft_mag, fft_phase = compute_fft_image(refimg)
+
+scale_factor = 0.6
+
+s = fft_mag.shape[0]
+
+#xmin = s*0.5*(1-scale_factor)
+#xmax = s 
+fft_mag = fft_mag[300:500, 300:500]
+fft_phase = fft_phase[300:500, 300:500]
+
+
+plt.imshow(np.sqrt(fft_mag**2+fft_phase**2))
+plt.colorbar()
+plt.show()
+
+#%%
+plt.clf()
+fft_reversed = np.empty(fft_mag.shape, dtype=complex)
+
+fft_reversed.real = fft_mag
+fft_reversed.imag = fft_phase
+
+fft_reversed_shift = ifftshift(fft_reversed)
+fft_reversed = ifft2(fft_reversed_shift)
+
+fft_reversed_mag = fft_reversed.real
+fft_reversed_phase = fft_reversed.imag
+
+plt.imshow(np.sqrt(fft_reversed_mag**2+fft_reversed_phase**2))
+plt.show()
+
+
+#%%
+
+#def calc_B_matrix(reference_dots, n_modes):
+#    
+#
+#    displacements = reference_dots - random_dots
+#    
 #    reference_dots_normalized = (reference_dots - np.mean(reference_dots, axis=0)) / np.ptp(reference_dots, axis=0)
 #    displacements_normalized = displacements / np.ptp(reference_dots, axis=0)
 #    displacements_normalized = displacements_normalized.flatten()
-    reference_dots_normalized = reference_dots
-    displacements_normalized = displacements.flatten()
-    
-    a, b = 2, 10
-    zernike_gradients = np.zeros((b-a, len(reference_dots)*2))
-    
-    for i in range(a, b):
-        j=0
-        n,m = noll2nm(i)
-        
-        for k in range(len(reference_dots)):
-            x = reference_dots_normalized[k, 0]
-            y = reference_dots_normalized[k, 1]
-            zernike_gradients[i-2, j] =  (zernike_cartesian(n, m, x+1e-5, y) - zernike_cartesian(n, m, x, y))/1e-5
-            zernike_gradients[i-2, j+1] =  (zernike_cartesian(n, m, x, y+1e-5) - zernike_cartesian(n, m, x, y))/1e-5
-            j +=2
-        
-    B = np.linalg.pinv(zernike_gradients.T)
-    coefficient = B @ displacements_normalized
-    
-    
-    x = np.linspace(-1, 1, 500)
-    y = np.linspace(-1, 1, 500)
-    X, Y = np.meshgrid(x, y)
-    phi = np.zeros((500, 500))
-    
-    for l in range(a,b):
-        n,m = noll2nm(l)
-        phi += coefficient[l-2]*zernike_cartesian(n, m, X, Y)
-    
-    plt.figure()
-    plt.imshow(phi, cmap='viridis')
-    plt.colorbar()
-    plt.scatter(reference_dots_normalized[:, 0]*250+250, reference_dots_normalized[:, 1]*250+250, s=2, c='red')
-    plt.scatter(random_dots[:, 0]*250+250, random_dots[:, 1]*250+250, s=2, c='black')
-
-    plt.show()
-    
-    
-    
-                             
-            
-            
-    
-    
+#    
+##    a, b = 2, 15
+#    zernike_gradients = np.zeros((n_modes, len(reference_dots)*2))
+#    
+#    for i in range(2, 2+n_modes):
+#        j=0
+#        n,m = noll2nm(i)
+#        
+#        for k in range(len(reference_dots)):
+#            x = reference_dots_normalized[k, 0]
+#            y = reference_dots_normalized[k, 1]
+#            zernike_gradients[i-2, j] =  (zernike_cartesian(n, m, x+1e-5, y) - zernike_cartesian(n, m, x, y))/1e-5
+#            zernike_gradients[i-2, j+1] =  (zernike_cartesian(n, m, x, y+1e-5) - zernike_cartesian(n, m, x, y))/1e-5
+#            j +=2
+#        
+#
+#B = calc_B_matrix(reference_dots, 13)
+#B = np.linalg.pinv(zernike_gradients.T)
+#coefficient = B @ displacements_normalized
+#    
+#    
+#x = np.linspace(-1, 1, 200)
+#y = np.linspace(-1, 1, 200)
+#X, Y = np.meshgrid(x, y)
+#phi = np.zeros((200, 200))
+#
+#for l in range(a,b):
+#    n,m = noll2nm(l)
+#    phi += coefficient[l-2]*zernike_cartesian(n, m, X, Y)
+#
+#plt.figure()
+#plt.imshow(phi)
+#plt.colorbar()
+#
+#
+#
+#                         
+#        
+#        
+#    
+#    
     
     
     
